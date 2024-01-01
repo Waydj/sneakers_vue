3a9d18ab6096ce680d5d96e037f393f29ee840bf
waydj <farhminait@gmail.com>
2024-01-01 23:46:34 +0200

    add sort and search components

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,26 @@ import MyHeader from '/src/components/MyHeader.vue'
     <MyHeader />
 
     <div class="p-10">
-      <h2 class="mb-8 text-3xl font-bold">Все кроссовки</h2>
+      <div class="flex items-center justify-between">
+        <h2 class="mb-8 text-3xl font-bold">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="rounded-md border px-3 py-2 outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевле)</option>
+            <option value="">По цене (дороже)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
+            <input
+              type="search"
+              placeholder="Поиск..."
+              class="rounded-md border py-2 pl-11 pr-4 outline-none focus:border-slate-500"
+            />
+          </div>
+        </div>
+      </div>
 
       <MyCardList />
     </div>
